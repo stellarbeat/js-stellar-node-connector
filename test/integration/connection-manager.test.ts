31780caa957ffe6253d8409eb877e3d872ed9fe7
pieterjan84 <pieterjan84@users.noreply.github.com>
2021-07-17 12:15:07 +0200

    expanded integration test

--- test/integration/connection-manager.test.ts
+++ test/integration/connection-manager.test.ts
@@ -1,6 +1,8 @@
 import {ConnectionManager, PeerNode} from "../../src";
 import ConnectionServer from "../../src/connection-server";
 import Connection from "../../src/connection/connection";
+import {xdr} from "stellar-base";
+import StellarMessage = xdr.StellarMessage;
 
 let server: ConnectionServer;
 let connectionManager: ConnectionManager;
@@ -9,23 +11,37 @@ let connectionToServer: Connection;
 beforeAll(() => {
     connectionManager = new ConnectionManager(true);
     server = connectionManager.createConnectionServer();
-    server.listen(11623, '127.0.0.1', undefined, () => {});
+    server.listen(11623, '127.0.0.1', undefined, () => {
+    });
     connectionToServer = connectionManager.connect(new PeerNode('127.0.0.1', 11623));
 })
-afterAll(()=> {
+afterAll(() => {
     server.close();
 })
 
 test("connect", (done) => {
+    let pingPongCounter = 0;
     server.on("connection", (connection) => {
         connection.on("connect", () => {
             console.log("Fully connected to client");
         });
+        connection.on("data", (stellarMessage: StellarMessage) => {
+            //pong
+            connection.sendStellarMessage(StellarMessage.getScpQuorumset(Buffer.alloc(32)))
+        })
     });
+
 
     connectionToServer
         .on('connect', () => {
             console.log('Fully connected to server');
-            done();
-        })
-} )
+            //ping
+            connectionToServer.sendStellarMessage(StellarMessage.getScpQuorumset(Buffer.alloc(32)))
+        }).on('data', (stellarMessage: StellarMessage) => {
+            pingPongCounter++;
+            if(pingPongCounter > 100)
+                done()
+            else
+                connectionToServer.sendStellarMessage(StellarMessage.getScpQuorumset(Buffer.alloc(32)))
+    })
+})
