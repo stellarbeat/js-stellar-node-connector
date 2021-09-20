59dd66d557721b839e6d275bb8db3335638c5d03
pieterjan84 <pieterjan84@users.noreply.github.com>
2021-09-20 10:14:16 +0200

    added missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,22 @@ export {Node} from './node';
 export {Connection} from './connection/connection';
 export {UniqueSCPStatementTransform} from './unique-scp-statement-transform';
 export {StellarMessageRouter, MessageTypeName} from './stellar-message-router';
-export {ScpBallot,SCPStatement,SCPStatementType,ScpStatementPledges,ScpStatementPrepare,ScpStatementConfirm,ScpStatementExternalize,ScpNomination} from './scp-statement-dto';
+export {
+    ScpBallot,
+    SCPStatement,
+    SCPStatementType,
+    ScpStatementPledges,
+    ScpStatementPrepare,
+    ScpStatementConfirm,
+    ScpStatementExternalize,
+    ScpNomination
+} from './scp-statement-dto';
 export {getConfigFromEnv} from './node-config';
-export {getPublicKeyStringFromBuffer, getIpFromPeerAddress, verifySCPEnvelopeSignature, getQuorumSetFromMessage} from './stellar-message-service'; //todo: separate package?
+export {
+    getPublicKeyStringFromBuffer,
+    createSCPEnvelopeSignature,
+    createStatementXDRSignature,
+    getIpFromPeerAddress,
+    verifySCPEnvelopeSignature,
+    getQuorumSetFromMessage
+} from './stellar-message-service'; //todo: separate package?
